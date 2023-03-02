2ec606933a374d02f9d3b89698469561dfe17a43
Kyle Huntsman <3432646+kylehuntsman@users.noreply.github.com>
2023-03-02 01:44:26 -0700

    feat: implement /v1/retrieval-events handler

--- cmd/recorder/main.go
+++ cmd/recorder/main.go
@@ -3,28 +3,30 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 
-	recorder "github.com/filecoin-project/lassie-event-recorder"
+	"github.com/filecoin-project/lassie-event-recorder/eventrecorder"
 	"github.com/ipfs/go-log/v2"
 )
 
 var logger = log.Logger("lassie/event_recorder/cmd")
 
 func main() {
+	log.SetAllLoggers(log.LevelInfo)
+	ctx := context.Background()
+
 	// TODO: add flags for all options eventually.
 	httpListenAddr := flag.String("httpListenAddr", "0.0.0.0:40080", "The HTTP server listen address in address:port format.")
-
 	flag.Parse()
 
-	r, err := recorder.NewRecorder(
-		recorder.WithHttpServerListenAddr(*httpListenAddr),
-	)
+	addrOpt := eventrecorder.WithHttpServerListenAddr(*httpListenAddr)
+	r, err := eventrecorder.NewEventRecorder(ctx, addrOpt)
 	if err != nil {
 		logger.Fatalw("Failed to instantiate recorder", "err", err)
 	}
-	ctx := context.Background()
+
 	if err = r.Start(ctx); err != nil {
 		logger.Fatalw("Failed to start recorder", "err", err)
 	}
@@ -32,10 +34,11 @@ func main() {
 	sch := make(chan os.Signal, 1)
 	signal.Notify(sch, os.Interrupt)
 	<-sch
+	fmt.Println()
 	logger.Info("Terminating...")
 	if err := r.Shutdown(ctx); err != nil {
 		logger.Warnw("Failed to shut down server.", "err", err)
 	} else {
-		logger.Info("Shut down server successfully.")
+		logger.Info("Shut down server successfully")
 	}
 }
