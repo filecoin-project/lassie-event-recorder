d71ffd9fc78c842182af9e6a9e56ce6ced023142
hannahhoward <hannah@hannahhoward.net>
2023-04-24 22:01:49 -0600

    feat(eventrecorder): complete aggregated event work

--- eventrecorder/recorder.go
+++ eventrecorder/recorder.go
@@ -3,6 +3,7 @@ package eventrecorder
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/filecoin-project/lassie/pkg/types"
 	"github.com/ipfs/go-log/v2"
@@ -105,47 +106,120 @@ func (r *EventRecorder) RecordAggregateEvents(ctx context.Context, events []Aggr
 	totalLogger := logger.With("total", len(events))
 
 	var batchQuery pgx.Batch
+	var batchRetrievalAttempts pgx.Batch
 	for _, event := range events {
+		var timeToFirstByte time.Duration
+		if event.TimeToFirstByte != "" {
+			timeToFirstByte, _ = time.ParseDuration(event.TimeToFirstByte)
+		}
+		var timeToFirstIndexerResult time.Duration
+		if event.TimeToFirstIndexerResult != "" {
+			timeToFirstIndexerResult, _ = time.ParseDuration(event.TimeToFirstIndexerResult)
+		}
 		query := `
 		INSERT INTO aggregate_retrieval_events(
 			instance_id,
 			retrieval_id,
 			storage_provider_id,
-			time_to_first_byte_ms,
+			time_to_first_byte,
 			bandwidth_bytes_sec,
+			bytes_transferred,
 			success,
 			start_time,
-			end_time
+			end_time,
+			time_to_first_indexer_result,
+			indexer_candidates_received,
+			indexer_candidates_filtered,
+			protocols_allowed,
+			protocols_attempted
 		)
-		VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+		VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
 		`
 		batchQuery.Queue(query,
 			event.InstanceID,
 			event.RetrievalID,
 			event.StorageProviderID,
-			event.TimeToFirstByte,
+			timeToFirstByte,
 			event.Bandwidth,
+			event.BytesTransferred,
 			event.Success,
 			event.StartTime,
 			event.EndTime,
+			timeToFirstIndexerResult,
+			event.IndexerCandidatesReceived,
+			event.IndexerCandidatesFiltered,
+			event.ProtocolsAllowed,
+			event.ProtocolsAttempted,
 		).Exec(func(ct pgconn.CommandTag) error {
 			rowsAffected := ct.RowsAffected()
 			switch rowsAffected {
 			case 0:
-				totalLogger.Warnw("Retrieval event insertion did not affect any rows", "event", event, "rowsAffected", rowsAffected)
+				totalLogger.Warnw("Aggregated event insertion did not affect any rows", "event", event, "rowsAffected", rowsAffected)
 			default:
-				totalLogger.Debugw("Inserted event successfully", "event", event, "rowsAffected", rowsAffected)
+				totalLogger.Debugw("Inserted aggregated event successfully", "event", event, "rowsAffected", rowsAffected)
 			}
 			return nil
 		})
+		attempts := make(map[string]string, len(event.RetrievalAttempts))
+		for storageProviderID, retrievalAttempt := range event.RetrievalAttempts {
+			attempts[storageProviderID] = retrievalAttempt.Error
+			var timeToFirstByte time.Duration
+			if retrievalAttempt.TimeToFirstByte != "" {
+				timeToFirstByte, _ = time.ParseDuration(retrievalAttempt.TimeToFirstByte)
+			}
+			query := `
+		  INSERT INTO retrieval_attempts(
+			  retrieval_id,
+			  storage_provider_id,
+			  time_to_first_byte,
+			  error
+		  )
+		  VALUES ($1, $2, $3, $4)
+		  `
+			batchRetrievalAttempts.Queue(query,
+				event.RetrievalID,
+				storageProviderID,
+				timeToFirstByte,
+				retrievalAttempt.Error,
+			).Exec(func(ct pgconn.CommandTag) error {
+				rowsAffected := ct.RowsAffected()
+				switch rowsAffected {
+				case 0:
+					totalLogger.Warnw("Retrieval attempt insertion did not affect any rows", "retrievalID", event.RetrievalID, "retrievalAttempt", retrievalAttempt, "storageProviderID", storageProviderID, "rowsAffected", rowsAffected)
+				default:
+					totalLogger.Debugw("Inserted retrieval attempt successfully", "retrievalID", event.RetrievalID, "retrievalAttempt", retrievalAttempt, "storageProviderID", storageProviderID, "rowsAffected", rowsAffected)
+				}
+				return nil
+			})
+		}
+		if r.cfg.metrics != nil {
+			r.cfg.metrics.HandleAggregatedEvent(ctx,
+				timeToFirstIndexerResult,
+				timeToFirstByte,
+				event.Success,
+				event.StorageProviderID,
+				event.StartTime,
+				event.EndTime,
+				int64(event.Bandwidth),
+				int64(event.BytesTransferred),
+				int64(event.IndexerCandidatesReceived),
+				int64(event.IndexerCandidatesFiltered),
+				attempts,
+			)
+		}
 	}
 	batchResult := r.db.SendBatch(ctx, &batchQuery)
 	err := batchResult.Close()
 	if err != nil {
-		totalLogger.Errorw("At least one retrieval event insertion failed", "err", err)
+		totalLogger.Errorw("At least one aggregated event insertion failed", "err", err)
 		return err
 	}
-
+	batchResult = r.db.SendBatch(ctx, &batchRetrievalAttempts)
+	err = batchResult.Close()
+	if err != nil {
+		totalLogger.Errorw("At least one retrieval attempt insertion failed", "err", err)
+		return err
+	}
 	totalLogger.Info("Successfully submitted batch event insertion")
 	return nil
 }
--- schema.sql
+++ schema.sql
@@ -11,12 +11,26 @@ create table if not exists retrieval_events(
 );
 
 create table if not exists aggregate_retrieval_events(
-  retrieval_id uuid not null,
+  retrieval_id uuid not null UNIQUE,
   instance_id character varying(64) not null,
   storage_provider_id character varying(256),
-  time_to_first_byte_ms integer,
-  bandwidth_bytes_sec integer,
+  time_to_first_byte int8,
+  bandwidth_bytes_sec int8,
+  bytes_transferred int8,
   success boolean not null,
   start_time timestamp with time zone not null,
-  end_time timestamp with time zone not null
+  end_time timestamp with time zone not null,
+  time_to_first_indexer_result int8,                      
+	indexer_candidates_received integer,
+	indexer_candidates_filtered integer,
+	protocols_allowed        varchar[256][],    
+	protocols_attempted      varchar[256][]
+);
+
+create table if not exists retrieval_attempts(
+  retrieval_id uuid not null,
+  storage_provider_id character varying(256),
+  time_to_first_byte int8,
+  error character varying(256),
+  FOREIGN KEY (retrieval_id) REFERENCES aggregate_retrieval_events (retrieval_id)
 );
