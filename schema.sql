8effd01a253ab76e714cd028422fe317f1aec1a0
Rod Vagg <rod@vagg.org>
2023-08-24 12:18:16 +1000

    fix: update schema to match current, update pgx dep

--- schema.sql
+++ schema.sql
@@ -16,25 +16,25 @@ create table if not exists aggregate_retrieval_events(
   url_path text,
   instance_id character varying(64) not null,
   storage_provider_id character varying(256),
-  time_to_first_byte int8,
-  bandwidth_bytes_sec int8,
-  bytes_transferred int8,
+  time_to_first_byte bigint,
+  bandwidth_bytes_sec bigint,
+  bytes_transferred bigint,
   success boolean not null,
   start_time timestamp with time zone not null,
   end_time timestamp with time zone not null,
-  time_to_first_indexer_result int8,                      
-	indexer_candidates_received integer,
-	indexer_candidates_filtered integer,
-	protocols_allowed        varchar[256][],    
-	protocols_attempted      varchar[256][],
-  protocol_succeeded       varchar[256]
+  time_to_first_indexer_result bigint,
+  indexer_candidates_received integer,
+  indexer_candidates_filtered integer,
+  protocols_allowed        character varying[],
+  protocols_attempted      character varying[],
+  protocol_succeeded       character varying(256)
 );
 
 create table if not exists retrieval_attempts(
   retrieval_id uuid not null,
   storage_provider_id character varying(256),
-  time_to_first_byte int8,
+  time_to_first_byte bigint,
   error text,
-  protocol varchar[256],
+  protocol character varying(256),
   FOREIGN KEY (retrieval_id) REFERENCES aggregate_retrieval_events (retrieval_id)
 );
